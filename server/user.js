cc738ada7ba148d997486659352635bd58cc220b
LYX <Rile14929@163.com>
2018-02-12 21:12:33 +0800

    boss 牛人完善信息

--- server/user.js
+++ server/user.js
@@ -12,6 +12,24 @@ Router.get('/list',function(req,res){
         return res.json(doc)
     })
 })
+
+Router.post('/update',function(req,res){
+    const userid = req.cookies.userid
+	if (!userid) {
+            return json.dumps({code:1})
+        }
+    const body = req.body
+    // 查找并且修改数据
+	User.findByIdAndUpdate(userid,body,function(err,doc){
+		const data = Object.assign({},{
+			user:doc.user,
+			type:doc.type
+		},body)
+		return res.json({code:0,data})
+	})
+
+})
+
 Router.post('/login',function(req,res){
     // console.log(req.body)
     const {user,pwd}=req.body
--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import Login from './container/login/login'
 import Register from './container/register/register'
 import reducers from './reducer'
 import AuthRoute from './component/authroute/authroute'
+import BossInfo from './container/bossinfo/bossinfo'
+import GeniusInfo from './container/geniusinfo/geniusinfo'
 import './config'
 import './index.css'
 
@@ -25,9 +27,14 @@ ReactDom.render(
         <BrowserRouter>
             <div>
                 <AuthRoute></AuthRoute>
-                <Route path='/boss' component={Boss}></Route>
-                <Route path='/login' component={Login}></Route>
-                <Route path='/register' component={Register}></Route>
+                <Switch>
+                    <Route path='/bossinfo' component={BossInfo}></Route>
+                    <Route path='/geniusinfo' component={GeniusInfo}></Route>
+                    <Route path='/boss' component={Boss}></Route>
+                    <Route path='/login' component={Login}></Route>
+                    <Route path='/register' component={Register}></Route>
+                    <Redirect to="/login"></Redirect>
+                </Switch>
             </div>
         </BrowserRouter>
     </Provider>),
